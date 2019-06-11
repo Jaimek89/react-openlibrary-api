e78c7b10ba288c57feb0a9853c58e203073b6ecb
Jaimek89 <jaimek89@gmail.com>
2019-06-11 12:12:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import Container from './container'
+
+import data from './data.json'
+
+const headings = ['When', 'Who', 'Description']
+
+class App extends Component {
+  render() {
+    return (
+      <Container data={data} title='OpenLibrary API' headings={headings} />
+    )
+  } 
 }
 
-export default App;
+export default App
